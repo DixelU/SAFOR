513243957410eac2ce6c82fa4333feae575a2f2a
DixelU <89090823215@yandex.ru>
2020-12-25 21:01:06 +0500

    Update download_boost_fast.py

--- download_boost_fast.py
+++ download_boost_fast.py
@@ -1,4 +1,4 @@
 import wget
 
-url = 'https://dl.bintray.com/boostorg/release/1.75.0/source/boost_1_75_0.zip';
+url = 'https://dl.bintray.com/boostorg/release/1.75.0/source/boost_1_75_0_rc1.zip';
 wget.download(url, 'boost.zip');
