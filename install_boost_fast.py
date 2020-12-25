22ce0732e0327285c250987532e36ae8b17f4802
DixelU <89090823215@yandex.ru>
2020-12-25 20:46:02 +0500

    Update install_boost_fast.py

--- install_boost_fast.py
+++ install_boost_fast.py
@@ -1,7 +1,4 @@
-from requests import get
-def download(url, file_name):
-  with open(file_name, "wb") as file:
-    response = get(url);
-    file.write(response.content);
-  
-download("https://dl.bintray.com/boostorg/release/1.75.0/source/boost_1_75_0.zip","boost.zip");");
+import wget
+
+url = 'https://dl.bintray.com/boostorg/release/1.75.0/source/boost_1_75_0.zip';
+wget.download(url, 'boost.zip');
