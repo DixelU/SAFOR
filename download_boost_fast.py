e06701f7c679760744ed9e806eb26d6d453dc704
DixelU <89090823215@yandex.ru>
2021-07-23 00:09:21 +0500

    updated boost lib

--- download_boost_fast.py
+++ download_boost_fast.py
@@ -1,4 +1,4 @@
 import wget
 
-url = 'https://dl.bintray.com/boostorg/release/1.75.0/source/boost_1_75_0_rc1.zip';
+url = 'https://boostorg.jfrog.io/artifactory/main/release/1.76.0/source/boost_1_76_0.zip';
 wget.download(url, 'boost.zip');
